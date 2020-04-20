abd1b91c97c8e75cbf07271261d9200a26a9bbac
chenhang <18730051277@163.com>
2020-04-20 17:16:55 +0800

    获取前程无忧职位信息
    Signed-off-by: chenhang <18730051277@163.com>

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@EnableScheduling //开启定时任务，定时抓取数据
 @MapperScan("com.test.demo.dao")
-@ComponentScan(value = "com.test")//包扫描
+//@ComponentScan(value = "com.test")//包扫描
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
